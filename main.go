222dbc49d1fe2c21547239cdc5b073db4cc134a4
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-09-15 21:23:47 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,55 +1,61 @@
 package main
 
 import (
-        "fmt"
-        "io/ioutil"
-        "os"
-        "path/filepath"
-        "strings"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	filePath  = "/home/example/example.json"
+	oldString = "old"
+	newString = "new"
 )
 
 func visit(path string, fi os.FileInfo, err error) error {
 
-        if err != nil {
-                return err
-        }
+	if err != nil {
+		return err
+	}
 
-        if !!fi.IsDir() {
-                return nil //
-        }
+	if !!fi.IsDir() {
+		return nil //
+	}
 
-        matched, err := filepath.Match("*.txt", fi.Name())
+	matched, err := os.Open((filePath))
 
-        if err != nil {
-                panic(err)
-                return err
-        }
+	if err != nil {
+		panic(err)
+		return err
+	}
 
-        if matched {
-                read, err := ioutil.ReadFile(path)
-                if err != nil {
-                        panic(err)
-                }
-                //fmt.Println(string(read))
-                fmt.Println(path)
-                // change it here.
-                newContents := strings.Replace(string(read), "old", "new", -1)
+	if matched {
+		read, err := ioutil.ReadFile(path)
+		if err != nil {
+			panic(err)
+		}
+		//fmt.Println(string(read))
+		fmt.Println(path)
+		// change it here.
+		newContents := strings.Replace(string(read), (oldString), (newString), -1)
 
-                fmt.Println(newContents)
+		fmt.Println(newContents)
 
-                err = ioutil.WriteFile(path, []byte(newContents), 0)
-                if err != nil {
-                        panic(err)
-                }
+		err = ioutil.WriteFile(path, []byte(newContents), 0)
+		if err != nil {
+			panic(err)
+		}
 
-        }
+	}
 
-        return nil
+	return nil
 }
 
 func main() {
-        err := filepath.Walk(".", visit)
-        if err != nil {
-                panic(err)
-        }
+	err := filepath.Walk(".", visit)
+	if err != nil {
+		panic(err)
+	}
 }
