3eda43eb610b884b39b2aaa7f4d571558d038063
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-09-17 19:26:45 +0000

    fixed things

--- main.go
+++ main.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-        "io/ioutil"
-        "log"
-        "strings"
+	"io/ioutil"
+	"log"
+	"strings"
 )
 
 const (
-        // the location of the file.
-        filePath = "./example.json"
+	// the location of the file.
+	filePath = "./example.json"
 )
 
 var (
-        // the string your trying to change.
-        oldString = "THIS IS THE OLD STRING."
-        // the new string you want to replace it with.
-        newString = "THIS IS THE NEW STRING."
+	// the string your trying to change.
+	oldString = "THIS IS THE OLD STRING."
+	// the new string you want to replace it with.
+	newString = "THIS IS THE NEW STRING."
 )
 
 func main() {
-        // lets read the file
-        read, err := ioutil.ReadFile(filePath)
+	// lets read the file
+	read, err := ioutil.ReadFile(filePath)
 
-        // if there is a error reading the file than we show the error using the log package
-        if err != nil {
-                log.Fatalln(err)
-        }
+	// if there is a error reading the file than we show the error using the log package
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-        // replace the current old string with the new string
-        newContents := strings.Replace(string(read), (oldString), (newString), -1)
+	// replace the current old string with the new string
+	newContents := strings.Replace(string(read), (oldString), (newString), -1)
 
-        // write the new string
-        err = ioutil.WriteFile(filePath, []byte(newContents), 0)
+	// write the new string
+	err = ioutil.WriteFile(filePath, []byte(newContents), 0)
 
-        // if there is a error print it
-        if err != nil {
-                log.Fatalln(err)
-        }
+	// if there is a error print it
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
