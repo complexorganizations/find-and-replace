200bfb343d3ddf2edc2ce3302316db2f569c65c9
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-10-01 22:13:35 -0400

    Update main.go

--- main.go
+++ main.go
@@ -21,18 +21,14 @@ var (
 func main() {
 	// lets read the file
 	read, err := ioutil.ReadFile(filePath)
-
 	// if there is a error reading the file than we show the error using the log package
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	// replace the current old string with the new string
 	newContents := strings.Replace(string(read), (oldString), (newString), -1)
-
 	// write the new string
 	err = ioutil.WriteFile(filePath, []byte(newContents), 0)
-
 	// if there is a error print it
 	if err != nil {
 		log.Fatalln(err)
